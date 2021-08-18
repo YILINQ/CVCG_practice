49b2c19236b237fbeddf52d3ea907eb5375dc2ac
YILINQ <victlin@outlook.com>
2021-08-19 01:52:45 +0800

    pose detection added, but has an ssl connection error

--- Utils/PoseDetectionUtils.py
+++ Utils/PoseDetectionUtils.py
@@ -1,2 +1,51 @@
 import cv2
-import mediapipe
+import mediapipe as mp
+
+
+class PoseDetector:
+    def __init__(self, mode=False, upBody=False, smooth=True, detectConf=0.5, trackingConf=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectCon = detectConf
+        self.trackingCon = trackingConf
+
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectCon, self.trackingCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks and draw:
+            self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+    def getPosition(self, img, draw=True):
+        lmList = []
+        if self.results:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append((id, cx, cy))
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = PoseDetector()
+    while True:
+        success, img = cap.read()
+
+        img = detector.findPose(img)
+        lmList = detector.getPosition(img)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
