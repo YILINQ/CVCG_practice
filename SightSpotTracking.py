f1c0432da5c9102f711292ad36e981b1c21f8529
YILINQ <victlin@outlook.com>
2021-08-25 02:20:07 +0800

    eye catch added

--- SightSpotTracking.py
+++ SightSpotTracking.py
@@ -1,5 +1,7 @@
 import json
-import keras
+import cv2
+import tensorflow as tf
+from tensorflow import keras
 from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten, Dropout
 from keras.models import Sequential
 import numpy as np
@@ -36,3 +38,27 @@ def create_model():
     model.compile(optimizer=keras.optimizers.Adam(0.0005), loss='mean_squared_error', metrics=['accuracy'])
 
     return model
+
+
+def eye_catch():
+    cap = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    while True:
+        success, img = cap.read()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+            roi_gray = gray[y: y + h, x: x + w]
+            roi_color = img[y: y + h, x: x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 2)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 1)
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
+
+
+eye_catch()
