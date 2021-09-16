98c92424a5a5df191fc20863b4a495fd36eab5b2
YILINQ <victlin@outlook.com>
2021-09-16 23:09:27 +0800

    nearest neighbor(best) and graph_cut method added

--- Image_quilting.py
+++ Image_quilting.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import heapq
+import PIL.Image as Image
 
 
 def random_patch(texture, block_size):
@@ -9,14 +11,77 @@ def random_patch(texture, block_size):
     return texture[i: i + block_size, j:j + block_size]
 
 
-def quilting(image, block_size, num_block, mode="random", sequence=False, smooth_factor=5):
-    texture = cv2.imread(image)
+def L2_diff(patch, block_size, overlap, res, y, x):
+    error = 0
+    if x > 0:
+        error += np.sum((patch[:, :overlap] - res[y: y + block_size, x:x + overlap]) ** 2)
+    if y > 0:
+        error += np.sum((patch[:overlap, :] - res[y: y + overlap, x:x + block_size]) ** 2)
+    if x > 0 and y > 0:
+        error -= np.sum((patch[:overlap, :overlap] - res[y: y + overlap, x:x + overlap]) ** 2)
+    return error
 
+
+def best_patch(texture, block_size, overlap, y, x, res):
+    h, w, c = texture.shape
+    errors = np.zeros((h - block_size, w - block_size))
+
+    for i in range(h - block_size):
+        for j in range(w - block_size):
+            patch = texture[i: i + block_size, j: j + block_size]
+            error = L2_diff(patch, block_size, overlap, res, y, x)
+            errors[i, j] = error
+    i, j = np.unravel_index(np.argmin(errors), errors.shape)
+    return texture[i: i + block_size, j:j + block_size]
+
+
+def cut_path(errors):
+    pq = [(error, [i]) for i, error in enumerate(errors[0])]
+    heapq.heapify(pq)
+
+    h, w = errors.shape
+    visited = set()
+    while pq:
+        error, path = heapq.heappop(pq)
+        current_length = len(path)
+        current_index = path[-1]
+
+        if current_length == h:
+            return path
+        for delta in range(-1, 0):
+            next_index = current_index + delta
+            if 0 <= next_index < w:
+                if (current_length, next_index) not in visited:
+                    cum_error = error + errors[current_length, next_index]
+                    heapq.heappush(pq, (cum_error, path + [next_index]))
+                    visited.add((current_length, next_index))
+
+
+def min_cut_path(patch, overlap, result, y, x):
+    patch = patch.copy()
+    dy, dx, dc = patch.shape
+    min_cut = np.zeros_like(patch, dtype=bool)
+
+    if x > 0:
+        left_error = np.sum((patch[:, :overlap] - result[y: y + dy, x:x + overlap]) ** 2, axis=2)
+        for i, j in enumerate(cut_path(left_error)):
+            min_cut[i, :j] = True
+    if y > 0:
+        top_error = np.sum((patch[:overlap, :] - result[y: y + overlap, x:x + dx]) ** 2, axis=2)
+        for j, i in enumerate(cut_path(top_error.T)):
+            min_cut[:i, j] = True
+
+    np.copyto(patch, result[y:y + dy, x:x + dx], where=min_cut)
+    return patch
+
+
+def quilting(image, block_size, num_block, mode="random", smooth_factor=5):
+    texture = cv2.imread(image, 3)
     overlap = block_size // smooth_factor
 
     # calculate output image size
     h, w = block_size * num_block - (num_block - 1) * overlap, block_size * num_block - (num_block - 1) * overlap
-    result = np.zeros((h, w, texture.shape[2]))
+    result = np.zeros((h, w, texture.shape[-1]), np.float32)
 
     for i in range(num_block):
         for j in range(num_block):
@@ -26,15 +91,24 @@ def quilting(image, block_size, num_block, mode="random", sequence=False, smooth
             if i == 0 and j == 0:
                 patch = random_patch(texture, block_size)
             else:
+                patch = random_patch(texture, block_size)
                 if mode == "random":
                     patch = random_patch(texture, block_size)
-
+                if mode == "best":
+                    patch = best_patch(texture, block_size, overlap, y, x, result)
+                if mode == "cut":
+                    patch = best_patch(texture, block_size, overlap, y, x, result)
+                    patch = min_cut_path(patch, overlap, result, y, x)
             # dummy statement
-            patch = random_patch(texture, block_size)
             result[y: y + block_size, x:x + block_size] = patch
+
+            # cv2.imshow("result", result)
+            # cv2.waitKey(0)
+    result = result.astype(np.uint8)
     cv2.imshow("test", result)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
-    quilting("strawberry.png", block_size=25, num_block=5)
+    quilting('/Users/yilin/Desktop/CV_and_CG_projects/Hand_Face_Pose_detection/strawberry_test.jpeg', block_size=100,
+             num_block=5, mode="cut")
