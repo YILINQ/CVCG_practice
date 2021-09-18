a0df9fd6e3949f0de007b362de855762661df4f5
YILINQ <victlin@outlook.com>
2021-09-18 20:47:06 +0800

    minor fix

--- Image_quilting.py
+++ Image_quilting.py
@@ -116,9 +116,10 @@ def main():
         print(len(sys.argv))
         assert len(sys.argv) == 5
         quilting(image=sys.argv[1], block_size=int(sys.argv[2]), num_block=int(sys.argv[3]), mode=sys.argv[4])
+        return True
     except:
         print("Usage: python image_quilting_texture.py [img_path] [block_size] [num_block] [mode]")
-
+        return False
 
 if __name__ == "__main__":
     import sys
